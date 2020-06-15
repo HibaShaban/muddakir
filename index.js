90c64584a6d2b228d8e42891e3b1d6f3649b1a83
Hiba Shaban <hibashaban@Admins-MacBook-Pro.local>
2020-06-14 22:00:53 -0400

    First offical release v1.0

---
+++ index.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+
+    // Global variables
+    let db;
+    let question_type;
+    let suraNum;
+    let questions;
+    let total_questions;
+    let counter;
+
+    let setValues = function() {
+        suraNum = $("#suraNum").val();
+
+        counter = 1;
+        questions = JSON.parse(JSON.stringify(question_type[suraNum].questions));
+        total_questions = questions.length;
+    }
+
+    $.getJSON("data/questions.json", function(json) {
+        db = json;
+        $.each(db.mahawir, function(key, value) {   
+            $('#suraNum').append($("<option></option>").attr("value", key).text(value.sura));
+        });
+        question_type = db.maqati;
+        setValues();
+    });
+
+    $("#next-btn").on("click", function() {
+        if(counter > total_questions) {
+            setValues();
+        }
+        // if (questions.length === 0) {
+        //     $("#question").html("Check back for questions later");
+        //     $("#ayat").html();
+        //     $("#questionNum").html();
+        //     return;
+        // }
+        let index = Math.floor(Math.random() * questions.length);
+        $("#question").html(questions[index].question);
+        $("#ayat").html(questions[index].ayat + " :الآية/الآيات");
+        $("#questionNum").html(counter+"/"+total_questions);
+        questions.splice(index,1);
+        counter += 1;
+    });
+    
+    $('select').on('change', function() {
+        setValues();
+    });
+    
+    $('input[type=radio][name=question]').change(function() {
+        switch(this.value) {
+            case 'topics':
+                question_type = db.topics;
+                break;
+            case 'mahawir':
+                question_type = db.mahawir;
+                break;
+            case 'maqati':
+                question_type = db.maqati;
+                break;
+        }
+        setValues();
+    });
+
+    $('#switchRoundedOutlinedDefault').click(function(){
+        if($(this).is(':checked')){
+            $("#ayat").show();
+        } else {
+            $("#ayat").hide();
+        }
+    });
+});
